56fe37ed66cafdf235fb470e17f29b4cff78c1dd
Виктория Абраменко <abramenko.victoria@gmail.com>
2023-09-30 09:22:50 +0300

    second file

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var a := readInteger('Введите год:');
+  //if (a mod 100 = 0) and (a mod 400 = 0) then print()
+  if (a mod 4 <> 0) then print('Год не високосный')
+  else if a mod 100 = 0 then
+    if a mod 400 = 0 then
+      print('Год високосный')
+    else print('Год не високосный')
+  else print('Год не високосный')
+end.
