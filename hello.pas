29de48f55d39f9cfd9e14f433342e66995f6a350
Виктория Абраменко <abramenko.victoria@gmail.com>
2023-09-30 09:10:14 +0300

    <3

--- hello.pas
+++ hello.pas
@@ -1,3 +1,7 @@
 ﻿begin
-print('Привет, мир!');
+var a := ReadInteger('');
+if (a >= 4) and (a <= 10) then print('Доброе утро, мир!')
+else if (a >= 11) and (a <= 16) then print('Добрый день, мир!')
+else if (a >= 17) and (a <= 22) then print('Доброй вечер, мир!')
+else print('Доброй ночи, мир!');
 end.
